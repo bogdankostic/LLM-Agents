a06596ed31c32a6829c6c423e5004041e92ec715
bogdankostic <bogdankostic@web.de>
2024-11-28 19:58:26 -0800

    Parametrize evaluation scripts

--- assess_performance.py
+++ assess_performance.py
@@ -1,4 +1,5 @@
 from ast import literal_eval
+import argparse
 
 from haystack_integrations.document_stores.weaviate.document_store import WeaviateDocumentStore
 from haystack_integrations.components.retrievers.weaviate import WeaviateBM25Retriever, WeaviateEmbeddingRetriever
@@ -28,46 +29,57 @@ def mean_reciprocal_rank(retrieved_docs, relevant_docs):
     return mrr / len(relevant_docs)
 
 
-# Initialize document store and retrievers
-document_store = WeaviateDocumentStore(url="http://localhost:8080")
-bm25_retriever = WeaviateBM25Retriever(document_store=document_store, top_k=100)
-embedding_retriever = WeaviateEmbeddingRetriever(document_store=document_store, top_k=100)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file_path", type=str)
+    parser.add_argument("--text_column", type=str)
+    parser.add_argument("--embedding_column", type=str)
+    args = parser.parse_args()
 
-# Load the dataset
-comments_df = pd.read_csv("data/reddit_comments_sample_with_embeddings.csv")
-comments_df["embedding"] = comments_df["embedding"].apply(literal_eval)
-comments_df["arxiv_id"] = comments_df["arxiv_id"].astype(str)
-
-# Retrieve articles
-bm25_results = []
-for post in tqdm(comments_df["clean_reddit_post"].values):
-    retrieved_docs = bm25_retriever.run(query=post)["documents"]
-    retrieved_ids = [doc.id for doc in retrieved_docs]
-    bm25_results.append(retrieved_ids)
-
-embedding_results = []
-for emb in tqdm(comments_df["embedding"].values):
-    retrieved_docs = embedding_retriever.run(query_embedding=emb)["documents"]
-    retrieved_ids = [doc.id for doc in retrieved_docs]
-    embedding_results.append(retrieved_ids)
+    file_path = args.file_path
+    text_column = args.text_column
+    embedding_column = args.embedding_column
 
 
-# # BM25 results
-bm25_recall_at_1 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=1)
-print(f"BM25 Recall@1: {bm25_recall_at_1}")
-bm25_recall_at_10 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=10)
-print(f"BM25 Recall@10: {bm25_recall_at_10}")
-bm25_recall_at_100 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=100)
-print(f"BM25 Recall@100: {bm25_recall_at_100}")
-bm25_mrr = mean_reciprocal_rank(bm25_results, comments_df["arxiv_id"].values)
-print(f"BM25 MRR: {bm25_mrr}")
+    # Initialize document store and retrievers
+    document_store = WeaviateDocumentStore(url="http://localhost:8080")
+    bm25_retriever = WeaviateBM25Retriever(document_store=document_store, top_k=100)
+    embedding_retriever = WeaviateEmbeddingRetriever(document_store=document_store, top_k=100)
 
-# Embedding results
-embedding_recall_at_1 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=1)
-print(f"Embedding Recall@1: {embedding_recall_at_1}")
-embedding_recall_at_10 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=10)
-print(f"Embedding Recall@10: {embedding_recall_at_10}")
-embedding_recall_at_100 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=100)
-print(f"Embedding Recall@100: {embedding_recall_at_100}")
-embedding_mrr = mean_reciprocal_rank(embedding_results, comments_df["arxiv_id"].values)
-print(f"Embedding MRR: {embedding_mrr}")
+    # Load the dataset
+    comments_df = pd.read_csv(file_path)
+    comments_df[embedding_column] = comments_df[embedding_column].apply(literal_eval)
+    comments_df["arxiv_id"] = comments_df["arxiv_id"].astype(str)
+
+    # Retrieve articles
+    bm25_results = []
+    for post in tqdm(comments_df[text_column].values):
+        retrieved_docs = bm25_retriever.run(query=post)["documents"]
+        retrieved_ids = [doc.id for doc in retrieved_docs]
+        bm25_results.append(retrieved_ids)
+
+    embedding_results = []
+    for emb in tqdm(comments_df[embedding_column].values):
+        retrieved_docs = embedding_retriever.run(query_embedding=emb)["documents"]
+        retrieved_ids = [doc.id for doc in retrieved_docs]
+        embedding_results.append(retrieved_ids)
+
+    # # BM25 results
+    bm25_recall_at_1 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=1)
+    print(f"BM25 Recall@1: {bm25_recall_at_1}")
+    bm25_recall_at_10 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=10)
+    print(f"BM25 Recall@10: {bm25_recall_at_10}")
+    bm25_recall_at_100 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=100)
+    print(f"BM25 Recall@100: {bm25_recall_at_100}")
+    bm25_mrr = mean_reciprocal_rank(bm25_results, comments_df["arxiv_id"].values)
+    print(f"BM25 MRR: {bm25_mrr}")
+
+    # Embedding results
+    embedding_recall_at_1 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=1)
+    print(f"Embedding Recall@1: {embedding_recall_at_1}")
+    embedding_recall_at_10 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=10)
+    print(f"Embedding Recall@10: {embedding_recall_at_10}")
+    embedding_recall_at_100 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=100)
+    print(f"Embedding Recall@100: {embedding_recall_at_100}")
+    embedding_mrr = mean_reciprocal_rank(embedding_results, comments_df["arxiv_id"].values)
+    print(f"Embedding MRR: {embedding_mrr}")
--- embed_posts.py
+++ embed_posts.py
@@ -1,25 +1,42 @@
+import argparse
+
 from sentence_transformers import SentenceTransformer
 import pandas as pd
 
 
 BATCH_SIZE = 16
 
-# Initialize the SentenceTransformer model
-embedder = SentenceTransformer(
-    model_name_or_path="BAAI/bge-small-en-v1.5",
-    device="mps"
-)
-# Load the dataset
-posts_df = pd.read_csv("data/reddit_comments_sample.csv")
 
-# Embed the posts
-embeddings = embedder.encode(
-    posts_df["clean_reddit_post"].tolist(),
-    normalize_embeddings=True,
-    batch_size=BATCH_SIZE,
-    show_progress_bar=True
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file_path", type=str)
+    parser.add_argument("--column_names", type=str, nargs="+")
+    args = parser.parse_args()
 
-# Save the embeddings
-posts_df["embedding"] = embeddings.tolist()
-posts_df.to_csv("data/reddit_comments_sample_with_embeddings.csv", index=False)
+    file_path = args.file_path
+    column_names = args.column_names
+
+    # Initialize the SentenceTransformer model
+    embedder = SentenceTransformer(
+        model_name_or_path="BAAI/bge-small-en-v1.5",
+        device="mps"
+    )
+    # Load the dataset
+    posts_df = pd.read_csv(file_path)
+
+    for column_name in column_names:
+        # Embed the posts
+        embeddings = embedder.encode(
+            posts_df[column_name].tolist(),
+            normalize_embeddings=True,
+            batch_size=BATCH_SIZE,
+            show_progress_bar=True
+        )
+
+        # Add the embeddings to the DataFrame
+        posts_df[f"{column_name}_embedding"] = embeddings.tolist()
+
+    # Save the embeddings
+    filename = file_path.split(".")[0]
+    posts_df.to_csv(f"{filename}_embeddings.csv", index=False)
+
