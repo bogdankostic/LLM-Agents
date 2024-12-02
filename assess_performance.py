51d2ce653ceea0643e49c025cfb0040637bd652a
bogdankostic <bogdankostic@web.de>
2024-12-02 10:58:51 -0800

    Add bootstrapping to eval

--- assess_performance.py
+++ assess_performance.py
@@ -5,6 +5,7 @@ from haystack_integrations.document_stores.weaviate.document_store import Weavia
 from haystack_integrations.components.retrievers.weaviate import WeaviateBM25Retriever, WeaviateEmbeddingRetriever
 import pandas as pd
 from tqdm import tqdm
+import numpy as np
 
 
 def recall(retrieved_docs, relevant_docs, top_k=None):
@@ -27,6 +28,15 @@ def mean_reciprocal_rank(retrieved_docs, relevant_docs):
                 break
 
     return mrr / len(relevant_docs)
+
+
+def calculate_metrics(predictions, labels):
+    recall_at_1 = recall(predictions, labels, top_k=1)
+    recall_at_10 = recall(predictions, labels, top_k=10)
+    recall_at_100 = recall(predictions, labels, top_k=100)
+    mrr = mean_reciprocal_rank(predictions, labels)
+
+    return recall_at_1, recall_at_10, recall_at_100, mrr
 
 
 if __name__ == "__main__":
@@ -64,22 +74,62 @@ if __name__ == "__main__":
         retrieved_ids = [doc.id for doc in retrieved_docs]
         embedding_results.append(retrieved_ids)
 
-    # # BM25 results
-    bm25_recall_at_1 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=1)
+    # Print metrics
+    print("Performance Metrics on Sampled Comments")
+    bm25_recall_at_1, bm25_recall_at_10, bm25_recall_at_100, bm25_mrr = calculate_metrics(
+        bm25_results, comments_df["arxiv_id"].values
+    )
+    embedding_recall_at_1, embedding_recall_at_10, embedding_recall_at_100, embedding_mrr = calculate_metrics(
+        embedding_results, comments_df["arxiv_id"].values
+    )
     print(f"BM25 Recall@1: {bm25_recall_at_1}")
-    bm25_recall_at_10 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=10)
     print(f"BM25 Recall@10: {bm25_recall_at_10}")
-    bm25_recall_at_100 = recall(bm25_results, comments_df["arxiv_id"].values, top_k=100)
     print(f"BM25 Recall@100: {bm25_recall_at_100}")
-    bm25_mrr = mean_reciprocal_rank(bm25_results, comments_df["arxiv_id"].values)
     print(f"BM25 MRR: {bm25_mrr}")
-
-    # Embedding results
-    embedding_recall_at_1 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=1)
     print(f"Embedding Recall@1: {embedding_recall_at_1}")
-    embedding_recall_at_10 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=10)
     print(f"Embedding Recall@10: {embedding_recall_at_10}")
-    embedding_recall_at_100 = recall(embedding_results, comments_df["arxiv_id"].values, top_k=100)
     print(f"Embedding Recall@100: {embedding_recall_at_100}")
-    embedding_mrr = mean_reciprocal_rank(embedding_results, comments_df["arxiv_id"].values)
     print(f"Embedding MRR: {embedding_mrr}")
+
+    # Do bootstrapping to get standard deviation
+    bm25_r_at_1 = []
+    bm25_r_at_10 = []
+    bm25_r_at_100 = []
+    bm25_mrr_values = []
+    embedding_r_at_1 = []
+    embedding_r_at_10 = []
+    embedding_r_at_100 = []
+    embedding_mrr_values = []
+    for idx in range(100):
+        np.random.seed(idx)
+        sampled_indices = np.random.choice(comments_df.index, len(comments_df), replace=True)
+        bootstrapped_arxiv_ids = comments_df["arxiv_id"].values[sampled_indices]
+
+        bm25_bootstrapped_results = [bm25_results[i] for i in sampled_indices]
+        cur_r_at_1, cur_r_at_10, cur_r_at_100, cur_mrr = calculate_metrics(
+            bm25_bootstrapped_results, bootstrapped_arxiv_ids
+        )
+        bm25_r_at_1.append(cur_r_at_1)
+        bm25_r_at_10.append(cur_r_at_10)
+        bm25_r_at_100.append(cur_r_at_100)
+        bm25_mrr_values.append(cur_mrr)
+
+        embedding_bootstrapped_results = [embedding_results[i] for i in sampled_indices]
+        cur_r_at_1, cur_r_at_10, cur_r_at_100, cur_mrr = calculate_metrics(
+            embedding_bootstrapped_results, bootstrapped_arxiv_ids
+        )
+        embedding_r_at_1.append(cur_r_at_1)
+        embedding_r_at_10.append(cur_r_at_10)
+        embedding_r_at_100.append(cur_r_at_100)
+        embedding_mrr_values.append(cur_mrr)
+
+    print("Performance Metrics on Sampled Comments with Bootstrapping")
+    print(f"BM25 Recall@1: {np.nanmean(bm25_r_at_1)} (std: {np.nanstd(bm25_r_at_1)})")
+    print(f"BM25 Recall@10: {np.nanmean(bm25_r_at_10)} (std: {np.nanstd(bm25_r_at_10)})")
+    print(f"BM25 Recall@100: {np.nanmean(bm25_r_at_100)} (std: {np.nanstd(bm25_r_at_100)})")
+    print(f"BM25 MRR: {np.nanmean(bm25_mrr_values)} (std: {np.nanstd(bm25_mrr_values)})")
+
+    print(f"Embedding Recall@1: {np.nanmean(embedding_r_at_1)} (std: {np.nanstd(embedding_r_at_1)})")
+    print(f"Embedding Recall@10: {np.nanmean(embedding_r_at_10)} (std: {np.nanstd(embedding_r_at_10)})")
+    print(f"Embedding Recall@100: {np.nanmean(embedding_r_at_100)} (std: {np.nanstd(embedding_r_at_100)})")
+    print(f"Embedding MRR: {np.nanmean(embedding_mrr_values)} (std: {np.nanstd(embedding_mrr_values)})")
